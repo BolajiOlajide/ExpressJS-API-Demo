04a19f7845636deac5a49179b17eb48a851c7c60
Chnonso Obiora <chinonso.obiora@gmail.com>
2017-12-26 14:38:57 +0100

    cleanup7

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# this ensures that if there were any errors that occured previously, then the code won't run #
 if [ $? -eq 0 ]
 then
-    docker info 3>null 1>null 2>null
+# this is to ensure that docker is running in the local machine #
+    docker info 3>/dev/null 1>/dev/null 2>/dev/null
     if [ $? -eq 0 ]
     then
         echo "docker existing"
